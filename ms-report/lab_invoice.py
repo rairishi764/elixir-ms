53feb9973a930dec401a7d9c054c94cda41c2324
rishirai <rai.rishi764@gmail.com>
2020-11-12 00:22:05 +0000

    clean up code

--- ms-report/lab_invoice.py
+++ ms-report/lab_invoice.py
@@ -138,8 +138,7 @@ def data():
                 , ('discount_amt',discount_amt), ('advance',advance), ('key',financial_years)])
 
         data_dict = dict ([('days',day_dict),('months',month_dict),('years',years_dict)])
-        
-#        print(data_dict)
+
         return (data_dict)
         
     except Error as e:
