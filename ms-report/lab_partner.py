82d649102d4496a11f74eaa8154251146c32a46f
rishirai <rai.rishi764@gmail.com>
2020-12-01 20:02:09 +0000

    updated partner api

--- ms-report/lab_partner.py
+++ ms-report/lab_partner.py
@@ -29,34 +29,41 @@ def data():
 
         days = 5
         j=0
+        consultant_list =[]
+        data = []
+        day=[]
+        month_data =[]
+        year_data = []
+        data ={}
         for consultant in consultants:
-           # print('###############')
             consultant_share = []
-            center_share = []
+            center_share = []          
+            doc_dict = {}
+            consultant_name = ''
             keys = []       
             consultant_share_val = 0
             for i in range(days):
                 iday = datetime.date.today() - datetime.timedelta(days=i)
                 consultant_share_day = 0
                 center_share_temp = 0
+                
                 for row in records:
                     if(iday == row[2]):
                         json_data =json.loads(row[5])
                         for obj1 in json_data:
                             if(consultant[1]==obj1['partner']):
-                                consultant_share_day=consultant_share_day+obj1['partner_share_value']
+                                consultant_name = obj1['partner']
+                                consultant_share_day = consultant_share_day+obj1['partner_share_value']
                             else:
-                                continue
-                keys.append(str(iday))
+                                continue                      
+                keys.append(str(iday.strftime('%A')))
                 consultant_share.append(consultant_share_day)
-            day_dict = dict([('dayshare',consultant_share), ('daykeys',keys)])
-            consultant = consultant +(day_dict,)
-            consultants[j] = consultant
-            j=j+1
-        #print(consultants)
+            doc_dict = dict([('share',consultant_share), ('keys',keys),('partner',consultant[1])])
+            day.append(doc_dict)
+            
 
         months = 3
-        j=0
+        doc_dict = {}
         for consultant in consultants:
            # print('###############')
             consultant_share = []
@@ -74,20 +81,20 @@ def data():
                         json_data =json.loads(row[5])
                         for obj1 in json_data:
                             if(consultant[1]==obj1['partner']):
-                                #print(obj1['partner'])
                                 consultant_share_month=consultant_share_month+obj1['partner_share_value']
                             else:
                                 continue
+
+                imonth = datetime.datetime.today().month - i
                 month = datetime.datetime.now() - relativedelta(months=i)
                 temp_key = month.strftime('%B')
                 keys.append(str(temp_key))
                 consultant_share.append(consultant_share_month)
-            day_dict = dict([('monthshare',consultant_share), ('keys',keys)])
-            consultant = consultant +(day_dict,)
-            consultants[j] = consultant
-            j=j+1
+            doc_dict = dict([('share',consultant_share), ('keys',keys),('partner',consultant[1])])
+            month_data.append(doc_dict)
+            
         #print(consultants)
-
+        financial_years =[]
         years = 3
         j=0
         for consultant in consultants:
@@ -110,17 +117,15 @@ def data():
                                 consultant_share_month=consultant_share_month+obj1['partner_share_value']
                             else:
                                 continue
-                #imonth = datetime.datetime.today().month - i
-                #financial_years.append(str(iyear)+'-'+str(iyear+1))
+                imonth = datetime.datetime.today().month - i
+                financial_years.append(str(iyear)+'-'+str(iyear+1))
                 keys.append(str(iyear)+'-'+str(iyear+1))
                 consultant_share.append(consultant_share_month)
-            day_dict = dict([('yearshare',consultant_share), ('keys',keys)])
-            consultant = consultant +(day_dict,)
-            consultants[j] = consultant
-            j=j+1
-        #print(consultants)
-        
-        return str(consultants)            
+            doc_dict = dict([('share',consultant_share), ('keys',keys),('partner',consultant[1])])
+            year_data.append(doc_dict)
+
+        data = dict([('day',day),('month',month_data),('year',year_data)])
+        return data            
 
     except Error as e:
         print("Error reading data from MySQL table", e)
